9bb46fafab6b45c3e141298a0e187cd63e67b39d
Tarasa24 <petr.gajdosik2424@gmail.com>
2019-11-06 15:14:35 +0100

    printing

--- FromReddit.py
+++ FromReddit.py
@@ -4,6 +4,7 @@ from sys import argv
 from os import getenv, environ
 from dotenv import load_dotenv
 from pathlib import Path
+from time import sleep
 
 
 def parsemsg(s):
@@ -32,7 +33,7 @@ def login(nickname, password, channel):
 
 def send_msg(msg):
     IRC.send(("PRIVMSG #{} : {}\n".format(CHANNEL, msg)).encode())
-    print(NICK + " -> " + msg)
+    print(NICK + ": " + msg)
 
 
 # Check arguments or .env
@@ -41,7 +42,8 @@ if len(argv) != 6:
   load_dotenv(dotenv_path=env_path)
   for e in ["CLIENTID", "CLIENTSEC", "NICK", "ACCESS_TOKEN", "CHANNEL"]:
     if e not in environ or getenv(e) is "":
-      print('Incorrect number of arguments or .env not configured')
+      print('> Incorrect number of arguments or .env not configured')
+      sleep(3)
       exit()
 
 # Set all the variables necessary to connect to Reddit
@@ -51,19 +53,24 @@ CLIENTSEC = getenv("CLIENTSEC") or argv[2]
 NICK = getenv("NICK") or argv[3]
 ACCESS_TOKEN = getenv("ACCESS_TOKEN") or argv[4]
 CHANNEL = getenv("CHANNEL") or argv[5]
+print(" > Variables loaded")
 
 # Connect to Reddit api
 reddit = Reddit(client_id=CLIENTID,
                 client_secret=CLIENTSEC,
                 user_agent="windows:FromReddit(https://github.com/Tarasa24/FromReddit):v0.1 (by /u/Tarasa24_CZE)")
+print(" > Praw initiliazed")
 
 # Open up a socket and login
 IRC = socket(AF_INET, SOCK_STREAM)
 IRC.connect(("irc.twitch.tv", 6667))
 IRC.setblocking(False)
 login(NICK, ACCESS_TOKEN, CHANNEL)
+print(" > Twitch IRC connected")
 
 history = [] # Array holding the history of pervious posts
+
+print(" > Listening for new messages")
 while True:
   try:
     buffer = IRC.recv(1024)
@@ -71,7 +78,7 @@ while True:
     if msg[1] == "PING":
       send_data("PONG tmi.twitch.tv\r\n")  # Answer with pong as per RFC 1459
     elif msg[1] == "PRIVMSG":
-      print(msg[0] + " -> " + msg[2])
+      print(msg[0] + ": " + msg[2])
       if msg[2] == "!question":
         random = None
         while True:
